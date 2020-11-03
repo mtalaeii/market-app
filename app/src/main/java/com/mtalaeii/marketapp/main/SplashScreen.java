909d9fd2e8ef345e812c6156899642b5e64f941b
mahdi <mahdi.talaee1379@gmail.com>
2020-11-03 16:05:16 +0330

    Splash screen added
    Signed-off-by: mahdi <mahdi.talaee1379@gmail.com>

---
+++ app/src/main/java/com/mtalaeii/marketapp/main/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.mtalaeii.marketapp.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.mtalaeii.marketapp.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+}
